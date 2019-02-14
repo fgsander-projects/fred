7de99837999a4dcfa22ec027468f5f6db9fdd867
ale_felipe_server <ale_felipe_server@localhost>
2019-02-14 21:29:02 +0000

    changue languajes

---
+++ config.bak.php
@@ -0,0 +1,36 @@
+<?php
+	$dbServer = 'localhost';
+	$dbUsername = 'root';
+	$dbPassword = 'Fgs@felipe!86';
+	$dbDatabase = 'frederico_morais';
+
+	$adminConfig = array(
+		'adminUsername' => "admin",
+		'adminPassword' => "\$2y\$10\$NNEseXD5A6yXKvUpsn/9Ou5Wh2KQ9C3JKAS4iqKxLMkuMux3vT8cK",
+		'notifyAdminNewMembers' => "1",
+		'defaultSignUp' => "0",
+		'anonymousGroup' => "anonymous",
+		'anonymousMember' => "guest",
+		'groupsPerPage' => "10",
+		'membersPerPage' => "10",
+		'recordsPerPage' => "10",
+		'custom1' => "Nome",
+		'custom2' => "Cargo",
+		'custom3' => "",
+		'custom4' => "",
+		'MySQLDateFormat' => "%d/%m/%Y",
+		'PHPDateFormat' => "j/n/Y",
+		'PHPDateTimeFormat' => "d/m/Y, h:i a",
+		'senderName' => "Membership management",
+		'senderEmail' => "admin@admin.com",
+		'approvalSubject' => "Seu cadastro foi aprovado",
+		'approvalMessage' => "Dear member,\r\n\r\nYour membership is now approved by the admin. You can log in to your account here:\r\nhttp://www.memoriafredericomorais.com.br/frederico\r\n\r\nRegards,\r\nAdmin",
+		'hide_twitter_feed' => "1",
+		'maintenance_mode_message' => "<b>Our website is currently down for maintenance</b><br>\r\nWe expect to be back in a couple hours. Thanks for your patience.",
+		'mail_function' => "mail",
+		'smtp_server' => "",
+		'smtp_encryption' => "",
+		'smtp_port' => "25",
+		'smtp_user' => "",
+		'smtp_pass' => ""
+	);
